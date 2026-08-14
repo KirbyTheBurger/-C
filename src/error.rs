5f8597d2f1fa69f30920e5bb01d000fe7c51598b
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-08-14 13:07:10 -0400

    add errors

---
+++ src/error.rs
@@ -0,0 +1,32 @@
+pub struct Error {
+    msg: String,
+    span: std::ops::Range<usize>,
+}
+
+impl Error {
+    pub fn report(&self, path: &str) {
+        report_error(path, &self.msg, self.span.clone());
+    }
+
+    pub fn new(msg: impl Into<String>, span: std::ops::Range<usize>) -> Error {
+        Error { msg: msg.into(), span }
+    }
+}
+
+fn report_error(path: &str, msg: &str, span: std::ops::Range<usize>) {
+    let source = &std::fs::read_to_string(path).unwrap();
+
+    ariadne::Report::build(
+        ariadne::ReportKind::Error,
+        (path, span.clone()),
+    )
+        .with_message(msg)
+        .with_label(
+            ariadne::Label::new((path, span))
+                .with_message("here")
+                .with_color(ariadne::Color::Red)
+        )
+        .finish()
+        .eprint((path, ariadne::Source::from(source)))
+        .unwrap();
+}
