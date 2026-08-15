a62cbeda71ea95eca1a3ae865a0a2f9a0eb23ebc
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-08-14 20:31:22 -0400

    adapt to Spanned<T>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use neg_c::lexer::tokenize;
 fn main() {
     let input = std::fs::read_to_string("test.nc").unwrap();
     match tokenize(&input) {
-        Ok(t) => t.iter().for_each(|t| print!("{:?}, ", t.token)),
+        Ok(t) => t.iter().for_each(|t| print!("{:?}, ", t.element)),
         Err(e) => e.iter().for_each(|e| e.report("test.nc")),
     }
 }
