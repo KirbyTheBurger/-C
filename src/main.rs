e4ea28ffa9f911f978b2733050126c0c5fad9e72
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-08-14 20:55:40 -0400

    add parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use neg_c::lexer::tokenize;
+const PATH: &str = "test.nc";
+
+use neg_c::{lexer::tokenize, parser::Parser};
 
 fn main() {
-    let input = std::fs::read_to_string("test.nc").unwrap();
-    match tokenize(&input) {
-        Ok(t) => t.iter().for_each(|t| print!("{:?}, ", t.element)),
-        Err(e) => e.iter().for_each(|e| e.report("test.nc")),
-    }
+    let input = std::fs::read_to_string(PATH).unwrap();
+
+    let tokens = match tokenize(&input) {
+        Ok(t) => {
+            t.iter().for_each(|t| print!("{:?}, ", t.element));
+            t
+        },
+        Err(e) => {
+            e.iter().for_each(|e| e.report(PATH));
+            return;
+        },
+    };
+
+    let _statements = match Parser::new(tokens).parse() {
+        Ok(s) => {
+            s.iter().for_each(|s| print!("{:?}, ", s.element));
+            s
+        },
+        Err(e) => {
+            e.iter().for_each(|e| e.report(PATH));
+            return;
+        }
+    };
 }
