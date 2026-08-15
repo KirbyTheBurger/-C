ecd17e9059087eadc1f264e35ebe3dda3eac1d63
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-08-14 20:31:09 -0400

    make fields public

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,6 @@ pub mod parser;
 
 #[derive(Debug, PartialEq)]
 pub struct Spanned<T: std::fmt::Debug + PartialEq> {
-    element: T,
-    span: std::ops::Range<usize>,
+    pub element: T,
+    pub span: std::ops::Range<usize>,
 }
