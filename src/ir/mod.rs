50f182f31ea8bd9b4c84fcdb62d5329ac39c1446
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-09-07 22:07:37 -0400

    add spanned helper method

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -1,9 +1,20 @@
+use crate::Spanned;
+
 pub mod builder;
 
 pub type VReg = usize;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Instr {
     LoadImm(VReg, u16),
     Print(VReg),
+}
+
+impl Instr {
+    fn with_span(self, span: std::ops::Range<usize>) -> Spanned<Self> {
+        Spanned {
+            element: self,
+            span,
+        }
+    }
 }
