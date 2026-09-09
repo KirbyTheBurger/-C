683fe0166204a55ac0154c287c8e759d7d2ba9d2
KirbyTheBurger <jamesverhoef2010@gmail.com>
2026-09-09 08:58:52 -0400

    add instructions and register map

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -1,10 +1,14 @@
-use std::rc::Rc;
+use std::{collections::{HashMap, HashSet}, rc::Rc, sync::Arc};
 
-use crate::{Spanned, ir::Instr};
+use crate::{Spanned, ir::{Instr, VReg}};
+
+pub type Reg = usize;
 
 pub struct Writer {
     instructions: Vec<Rc<Spanned<Instr>>>,
     pos: usize,
+    free_regs: HashSet<Reg>,
+    vreg_map: HashMap<VReg, Reg>,
 
     output: String,
 }
@@ -14,12 +18,14 @@ impl Writer {
         Self {
             instructions: instructions.into_iter().map(|i| Rc::new(i)).collect(),
             pos: 0,
+            free_regs: HashSet::new(),
+            vreg_map: HashMap::new(),
 
             output: String::new(),
         }
     }
 
-    pub fn write(&mut self) -> String {
+    pub fn process(&mut self) -> String {
         while let Some(instruction) = self.current() {
             self.process_instruction(instruction);
             self.advance();
@@ -29,16 +35,37 @@ impl Writer {
     }
 
     fn process_instruction(&mut self, instruction: Rc<Spanned<Instr>>) {
-        match instruction {
+        match instruction.element {
+            Instr::LoadImm(vreg, n) => {
+                let reg = self.get_reg(vreg).expect("no free regs");
+                self.write(format!("LD {reg}, {n}"));
+            },
+            Instr::Print(vreg) => {
+                let reg = self.get_reg(vreg).expect("no free regs");
+                self.write(format!("OUT {reg}"));
+            }
             _ => todo!()
         }
     }
 
-    fn write_str(&mut self, src: impl ToString) {
-        self.output.extend(src.to_string().chars());
+    fn get_reg(&mut self, vreg: VReg) -> Option<usize> {
+        if let Some(reg) = self.vreg_map.get(&vreg) {
+            return Some(*reg);
+        }
+
+        let reg = self.free_regs.iter().next().copied();
+        if let Some(r) = reg {
+            self.free_regs.remove(&r);
+        }
+        reg
     }
 
-    fn newline(&mut self) {
+    fn free_reg(&mut self, reg: Reg) {
+        self.free_regs.insert(reg);
+    }
+
+    fn write(&mut self, src: impl Into<Arc<str>>) {
+        self.output.extend(src.into().chars());
         self.output.extend("\n".chars());
     }
 
